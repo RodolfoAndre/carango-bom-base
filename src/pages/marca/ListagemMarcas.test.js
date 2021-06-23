55b09af6f636a2e6da438ee573dfccc279a48a8c
Felipe Torres Magalhães <felipetorresmagalhaes@MacBook-Pro-de-Felipe.local>
2021-06-23 17:58:06 -0300

    Adiciona testes para o component ListagemMarca

---
+++ src/pages/marca/ListagemMarcas.test.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import ListagemMarcas from './ListagemMarcas';
+import MarcaService from '../../services/MarcaService';
+import CadastroMarca from './CadastroMarca';
+
+jest.mock('../../services/MarcaService');
+
+const renderComponent = () => {
+  return render(<ListagemMarcas />);
+};
+
+// TODO revisar quando tivermos implementado a feature de autenticação
+describe('Component de ListagemMarcas', () => {
+  describe('Quando entrar na página', () => {
+    it('deve exibir datagrid', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByRole } = renderComponent();
+      expect(await findByRole('grid')).toBeInTheDocument();
+    });
+
+    it('datagrid deve exibir coluna de marca', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByText } = renderComponent();
+      expect(await findByText('Marca')).toBeInTheDocument();
+    });
+
+    it('datagrid deve exibir linha com valores de marca', async () => {
+      MarcaService.listar.mockResolvedValue([
+        {
+          id: 34,
+          nome: 'FORD',
+        },
+      ]);
+      const { findByText } = renderComponent();
+      expect(await findByText('FORD')).toBeInTheDocument();
+    });
+
+    it('botão excluir deve ser exibido', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByText } = renderComponent();
+      expect(await findByText('Excluir')).toBeInTheDocument();
+    });
+
+    it('botão alterar deve ser exibido', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByText } = renderComponent();
+      expect(await findByText('Alterar')).toBeInTheDocument();
+    });
+
+    it('botão adicionar deve ser exibido', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByLabelText } = renderComponent();
+      expect(await findByLabelText('add')).toBeInTheDocument();
+    });
+  });
+
+  describe('O botão excluir deve', () => {
+    it('estar desabilitado caso nenhuma linha do DataGrid tenha sido selecionada', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByText } = renderComponent();
+      const botaoExcluir = (await findByText('Excluir')).parentElement;
+      expect(botaoExcluir).toBeDisabled();
+    });
+
+    it('estar habilitado caso uma linha do DataGrid tenha sido selecionada', async () => {
+      MarcaService.listar.mockResolvedValue([
+        {
+          id: 34,
+          nome: 'FORD',
+        },
+      ]);
+      const { findByText } = renderComponent();
+      const row = await findByText('FORD');
+      fireEvent.click(row);
+
+      const botaoExcluir = (await findByText('Excluir')).parentElement;
+      expect(botaoExcluir).not.toBeDisabled();
+    });
+  });
+
+  describe('O botão alterar deve', () => {
+    it('estar desabilitado caso nenhuma linha do DataGrid tenha sido selecionada', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByText } = renderComponent();
+      const botaoExcluir = (await findByText('Alterar')).parentElement;
+      expect(botaoExcluir).toBeDisabled();
+    });
+
+    it('estar habilitado caso uma linha do DataGrid tenha sido selecionada', async () => {
+      MarcaService.listar.mockResolvedValue([
+        {
+          id: 34,
+          nome: 'FORD',
+        },
+      ]);
+      const { findByText } = renderComponent();
+      const row = await findByText('FORD');
+      fireEvent.click(row);
+
+      const botaoAlterar = (await findByText('Alterar')).parentElement;
+      expect(botaoAlterar).not.toBeDisabled();
+    });
+  });
+
+  describe('O botão adicionar deve', () => {
+    it('estar habilitado', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByLabelText } = renderComponent();
+      const botaoAdicionar = await findByLabelText('add');
+      expect(botaoAdicionar).not.toBeDisabled();
+    });
+
+    it('rotear para a página de cadastrar marca', async () => {
+      MarcaService.listar.mockResolvedValue([]);
+      const { findByLabelText, findByText } = render(
+        <MemoryRouter initialEntries={['/listar-marcas']}>
+          <ListagemMarcas />
+          <CadastroMarca />
+        </MemoryRouter>
+      );
+      const botaoAdicionar = await findByLabelText('add');
+      fireEvent.click(botaoAdicionar);
+      expect(await findByText('Cadastrar')).toBeInTheDocument();
+    });
+  });
+});
