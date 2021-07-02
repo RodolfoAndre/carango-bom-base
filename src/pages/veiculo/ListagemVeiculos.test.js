2841bd32c0cbf12c254f919d0e6663981b7f3ccc
Louise Silva <louise@MacBook-Pro-de-Louise.local>
2021-07-02 18:18:45 -0300

    Adiciona mais testes de ListagemVeiculos

--- src/pages/veiculo/ListagemVeiculos.test.js
+++ src/pages/veiculo/ListagemVeiculos.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
 import ListagemVeiculos from './ListagemVeiculos';
 import VeiculoService from '../../services/VeiculoService';
 
@@ -9,72 +13,175 @@ const createRender = () => {
   return render(<ListagemVeiculos />);
 };
 
+const renderWithRouter = (history) => {
+  return render(
+    <Router history={history}>
+      <ListagemVeiculos />
+    </Router>,
+  );
+};
+
 describe('Component de ListagemVeiculos', () => {
+  beforeEach(() => {
+    VeiculoService.listar.mockResolvedValue([]);
+  });
+
+  const history = createMemoryHistory();
+
+  const veiculoSobTest = {
+    id: 134,
+    marca: 'FORD',
+    modelo: 'KA 123456',
+    ano: 2021,
+    valor: 80000,
+  };
+
   describe('Quando entrar na página', () => {
-    it('deve exibir datagrid', async () => {
-      VeiculoService.listar.mockResolvedValue([]);
-      const { findByRole } = createRender();
-      expect(await findByRole('grid')).toBeInTheDocument();
+    describe('Datagrid', () => {
+      it('deve ser exibido', async () => {
+        const { findByRole } = createRender();
+        const grid = await findByRole('grid');
+        expect(grid).toBeInTheDocument();
+      });
+
+      it('deve exibir colunas de marca, modelo, ano e valor', async () => {
+        const { findByText } = createRender();
+
+        const marca = await findByText('Marca');
+        const modelo = await findByText('Modelo');
+        const ano = await findByText('Ano');
+        //const valor = await findByText('Valor');
+
+        expect(marca).toBeInTheDocument();
+        expect(modelo).toBeInTheDocument();
+        expect(ano).toBeInTheDocument();
+        //expect(valor).toBeInTheDocument();
+      });
+
+      it('deve exibir linha com valores de marca, modelo, ano e valor', async () => {
+        VeiculoService.listar.mockResolvedValue([veiculoSobTest]);
+        const { findByText } = createRender();
+
+        const marca = await findByText(veiculoSobTest.marca);
+        const modelo = await findByText(veiculoSobTest.modelo);
+        const ano = await findByText(veiculoSobTest.ano);
+        //const valor = await findByText(veiculoSobTest.valor);
+
+        expect(marca).toBeInTheDocument();
+        expect(modelo).toBeInTheDocument();
+        expect(ano).toBeInTheDocument();
+        //expect(valor).toBeInTheDocument();
+      });
     });
 
-    it('datagrid deve exibir colunas de marca, modelo, ano e valor', async () => {
-      VeiculoService.listar.mockResolvedValue([]);
-      const { getByText, findByText } = createRender();
-      expect(await findByText('Marca')).toBeInTheDocument();
-      expect(getByText('Modelo')).toBeInTheDocument();
-      expect(getByText('Ano')).toBeInTheDocument();
-      //expect(await findByText('Valor')).toBeInTheDocument();
-    });
+    describe('Botão adicionar', () => {
+      it('deve ser exibido', async () => {
+        const { findByLabelText } = createRender();
+        const botaoAdicionar = await findByLabelText('add');
+        expect(botaoAdicionar).toBeInTheDocument();
+      });
 
-    it('datagrid deve exibir linha com valores de marca, modelo, ano e valor', async () => {
-      VeiculoService.listar.mockResolvedValue([
-        {
-          id: 134,
-          modelo: 'KA 123456',
-          ano: 2021,
-          valor: 80000.0,
-          marca: 'FORD',
-        },
-      ]);
-      const { findByText, getByText } = createRender();
-      expect(await findByText('FORD')).toBeInTheDocument();
-      expect(getByText('KA 123456')).toBeInTheDocument();
-      expect(getByText('2021')).toBeInTheDocument();
-      //expect(getByText('80000')).toBeInTheDocument();
+      it('deve estar habilitado', async () => {
+        const { findByLabelText } = createRender();
+        const botaoAdicionar = await findByLabelText('add');
+        expect(botaoAdicionar).not.toBeDisabled();
+      });
     });
   });
 
   // TODO revisar quando tivermos implementado a feature de autenticação
   describe('Quando um usuário estiver logado', () => {
-    it('Botão excluir deve ser exibido', async () => {
-      VeiculoService.listar.mockResolvedValue([]);
-      const { findByText } = createRender();
-      expect(await findByText('Excluir')).toBeInTheDocument();
+    describe('Botão excluir e alterar', () => {
+      it('devem ser exibidos', async () => {
+        const { findByText } = createRender();
+        const botaoExcluir = await findByText('Excluir');
+        const botaoAlterar = await findByText('Alterar');
+
+        expect(botaoExcluir).toBeInTheDocument();
+        expect(botaoAlterar).toBeInTheDocument();
+      });
+
+      it('devem estar desabilitados caso nenhuma linha do DataGrid tenha sido selecionada', async () => {
+        const { findByText } = createRender();
+
+        const botaoExcluir = (await findByText('Excluir')).parentElement;
+        const botaoAlterar = (await findByText('Alterar')).parentElement;
+
+        expect(botaoExcluir).toBeDisabled();
+        expect(botaoAlterar).toBeDisabled();
+      });
+
+      it('devem estar habilitados caso uma linha do DataGrid tenha sido selecionada', async () => {
+        VeiculoService.listar.mockResolvedValue([veiculoSobTest]);
+
+        const { findByText } = createRender();
+        const row = await findByText(veiculoSobTest.marca);
+        fireEvent.click(row);
+
+        const botaoExcluir = (await findByText('Excluir')).parentElement;
+        const botaoAlterar = (await findByText('Alterar')).parentElement;
+
+        expect(botaoExcluir).not.toBeDisabled();
+        expect(botaoAlterar).not.toBeDisabled();
+      });
     });
 
-    it('Botão excluir deve estar desabilitado caso nenhuma linha do DataGrid tenha sido selecionada', async () => {
-      VeiculoService.listar.mockResolvedValue([]);
-      const { findByText } = createRender();
-      const botaoExcluir = (await findByText('Excluir')).parentElement;
-      expect(botaoExcluir).toBeDisabled();
-    });
+    describe('Ao clicar no botão', () => {
+      describe('Excluir', () => {
+        it('deve chamar a função excluir, limpar o veículo selecionado e chamar a função listar', async () => {
+          VeiculoService.listar = jest.fn();
+          VeiculoService.listar.mockResolvedValue([veiculoSobTest]);
+          VeiculoService.excluir = jest.fn(() => Promise.resolve({ data: {} }));
 
-    it('Botão excluir deve estar habilitado caso uma linha do DataGrid tenha sido selecionada', async () => {
-      VeiculoService.listar.mockResolvedValue([
-        {
-          id: 134,
-          modelo: 'KA 123456',
-          ano: 2021,
-          valor: 80000.0,
-          marca: 'FORD',
-        },
-      ]);
-      const { findByText } = createRender();
-      const row = await findByText('FORD');
-      fireEvent.click(row);
+          const { findByText } = createRender();
+          const row = await findByText(veiculoSobTest.marca);
+          fireEvent.click(row);
 
-      const botaoExcluir = (await findByText('Excluir')).parentElement;
-      expect(botaoExcluir).not.toBeDisabled();
+          const botaoExcluir = (await findByText('Excluir')).parentElement;
+          fireEvent.click(botaoExcluir);
+
+          await waitFor(() => {
+            expect(VeiculoService.excluir).toHaveBeenCalledWith(veiculoSobTest);
+            expect(VeiculoService.excluir).toHaveBeenCalledTimes(1);
+          });
+
+          expect(botaoExcluir).toBeDisabled();
+          expect(VeiculoService.listar).toHaveBeenCalledTimes(2);
+        });
+      });
+
+      describe('Alterar', () => {
+        it('deve redirecionar para a rota de alteração de veículo', async () => {
+          VeiculoService.listar.mockResolvedValue([veiculoSobTest]);
+          history.push = jest.fn();
+
+          const { findByText } = renderWithRouter(history);
+          const row = await findByText(veiculoSobTest.marca);
+          fireEvent.click(row);
+
+          const botaoAlterar = (await findByText('Alterar')).parentElement;
+          fireEvent.click(botaoAlterar);
+
+          expect(history.push).toHaveBeenCalledTimes(1);
+          expect(history.push).toHaveBeenCalledWith(
+            `/alteracao-veiculo/${veiculoSobTest.id}`,
+          );
+        });
+      });
+
+      describe('Adicionar', () => {
+        it('deve redirecionar para a página de cadastro de veículo', async () => {
+          history.push = jest.fn();
+
+          const { findByLabelText } = renderWithRouter(history);
+
+          const botaoAdicionar = await findByLabelText('add');
+          fireEvent.click(botaoAdicionar);
+
+          expect(history.push).toHaveBeenCalledTimes(1);
+          expect(history.push).toHaveBeenCalledWith('/cadastro-veiculo');
+        });
+      });
     });
   });
 });
